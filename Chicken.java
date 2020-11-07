11953e6988c7b353654eb3f6e171e215a8b43fd0
Andy J Rivera <rivera.andy40@gmail.com>
2020-11-07 01:50:44 -0500

    Update Chicken.java

--- Chicken.java
+++ Chicken.java
@@ -2,12 +2,31 @@ package sample;
 
 public class Chicken extends Sandwich {
 
-    public abstract double price(){
+    public double price(){
+        return 8.99 + (extras.size() * PER_EXTRA);
+    }
 
+    @Override
+    public String basicIngredients() {
+        return "Fried Chicken, Spicy Sauce, Pickles";
+    }
+
+    public String extraIngredients(){
+        return super.extraIngredients();
     }
 
     @Override
     public String toString(){
+       return super.toString();
+    }
 
+    @Override
+    public boolean add(Object obj) {
+        return super.add(obj);
+    }
+
+    @Override
+    public boolean remove(Object obj) {
+        return super.remove(obj);
     }
 }
