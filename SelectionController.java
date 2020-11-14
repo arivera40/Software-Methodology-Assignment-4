6037a282f255d06f303f4e7de141612e9972e755
Andy J Rivera <rivera.andy40@gmail.com>
2020-11-13 22:53:24 -0500

    Update SelectionController.java

--- SelectionController.java
+++ SelectionController.java
@@ -1,16 +1,10 @@
 package sample;
 
+import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import  javafx.scene.control.Button;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-
-import com.sun.javafx.logging.Logger;
-import com.sun.javafx.logging.PlatformLogger.Level;
-
-import javafx.beans.Observable;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -19,17 +13,38 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.ListView;
 import javafx.scene.control.SelectionMode;
-import javafx.scene.control.TextArea;
+
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.text.Text;
+
 import javafx.stage.Stage;
+
+import java.text.DecimalFormat;
+
 
 public class SelectionController {
 
-    private ObservableList<String> meatSelection;
+    Image chickenImage = new Image("file:/C:/Users/river/IdeaProjects/OrderSandwichGUI/out/production/OrderSandwichGUI/sample/images/chicken.jpg");
+    Image beefImage = new Image("file:/C:/Users/river/IdeaProjects/OrderSandwichGUI/out/production/OrderSandwichGUI/sample/images/beef.jpg");
+    Image fishImage = new Image("file:/C:/Users/river/IdeaProjects/OrderSandwichGUI/out/production/OrderSandwichGUI/sample/images/fish.jpg");
 
-    private ObservableList<Extra> extraIngredients;
+    private ObservableList<Extra> extraObservableList = FXCollections.observableArrayList(
+            Extra.Lettuce,
+            Extra.Mayonnaise,
+            Extra.Tomatoes,
+            Extra.Ketchup,
+            Extra.Cheese,
+            Extra.Onions,
+            Extra.Tuna,
+            Extra.Oil,
+            Extra.Vinegar,
+            Extra.Avocado
+    );
+
+    String prevSandwich = "Chicken";
+
+    private ObservableList<Extra> currentObservableList = FXCollections.observableArrayList();
 
     @FXML
     private ComboBox meatBox;
@@ -50,10 +65,23 @@ public class SelectionController {
     private Button orderButton;
 
     @FXML
+    private Button addButton;
+
+    @FXML private Button removeButton;
+
+    @FXML
     private TextField priceField;
 
     @FXML
     private TextField orderNumberField;
+
+    private double price = 8.99;
+
+    private String pattern = "0.00";
+
+    private int orderNumber = 1;
+
+    protected static Order orders = new Order();
 
     @FXML
     void initialize(){
@@ -67,226 +95,146 @@ public class SelectionController {
         );
         meatBox.setValue("Chicken");
 
-        extraListView.getItems().addAll(
-                Extra.Lettuce,
-                Extra.Mayonnaise,
-                Extra.Tomatoes,
-                Extra.Ketchup,
-                Extra.Cheese,
-                Extra.Onions,
-                Extra.Tuna,
-                Extra.Oil,
-                Extra.Vinegar,
-                Extra.Avocado
-        );
+        //Sets default price and order number fields
+        priceField.setText("$" + formatPrice(price));
+        orderNumberField.appendText("" + orderNumber);
 
-
-
-//        extraIngredients.add(Extra.Lettuce);
-//        extraIngredients.add(Extra.Mayonnaise);
-//        extraIngredients.add(Extra.Tomatoes);
-//        extraIngredients.add(Extra.Ketchup);
-//        extraListView.setItems(extraIngredients);
+        extraListView.setItems(extraObservableList);
+        currentListView.setItems(currentObservableList);
+        extraListView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+        currentListView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
     }
 
+    @FXML
+    void addIngredients(ActionEvent event){
+        int currentListSize = currentObservableList.size();
+        currentObservableList.addAll(extraListView.getSelectionModel().getSelectedItems());
+        extraObservableList.removeAll(extraListView.getSelectionModel().getSelectedItems());
+        int itemsAdded = currentObservableList.size() - currentListSize;
+        double priceChange = 1.99 * itemsAdded;
+        price += priceChange;
+        priceField.setText("$" + formatPrice(price));
+        return;
+    }
 
-//
-//    @FXML
-//    private ListView ingred2;
-//
-//    @FXML
-//    private Button button1;
-//
-//    @FXML
-//    private Button button2;
-//
-//    @FXML
-//    private Button button_ingred;
-//
-//    @FXML
-//    private ImageView food_image;
-//
-//    @FXML
-//    private TextArea price;
-//
-//
-//    @FXML
-//    void addThem(ActionEvent event) {
-//
-//
-//        ObservableList<String> observableList = FXCollections.observableList(RightList);
-//        String r = ingredients.getSelectionModel().getSelectedItems().toString();
-//        r = r.substring(1, r.length() - 1);
-//        RightList.add(r);
-//
-//        Chicken chick = new Chicken();
-//
-//        ingred2.setItems(observableList);
-//
-//        /**
-//         *  a lot of random bs here messing with the program!!!!
-//         */
-//
-//        Order neworder = new Order();
-//        OrderLine zuzu = new OrderLine(neworder);
-//
-//
-//
-//        if(RightList.contains(extraList.get(0))) {
-//            //	System.out.println(Extra.Lettuce);
-//            //System.out.println(daisy);
-//            //	chick.chicken_test.add(Extra.Mayonnaise);
-//            chick.add(Avocado);
-//            //System.out.println(zuzu.getPrice());
-//            //extras.add(Extra.Avocado);
-//            //System.out.println(extras);
-//        }
-//        if(RightList.contains(extraList.get(1))) {
-//            System.out.println(Extra.Mayonnaise);
-//            //extras.add(Extra.Mayonnaise);
-//        }
-//        if(RightList.contains(extraList.get(2))) {
-//            System.out.println(Extra.Tomatoes);
-//        }
-//        if(RightList.contains(extraList.get(3))) {
-//            System.out.println(Extra.Ketchup);
-//        }
-//        if(RightList.contains(extraList.get(4))) {
-//            System.out.println(Extra.Cheese);
-//        }
-//        if(RightList.contains(extraList.get(5))) {
-//            System.out.println(Extra.Onions);
-//        }
-//        if(RightList.contains(extraList.get(6))) {
-//            System.out.println(Extra.Tuna);
-//        }
-//        if(RightList.contains(extraList.get(7))) {
-//            System.out.println(Extra.Oil);
-//        }
-//        if(RightList.contains(extraList.get(8))) {
-//            System.out.println(Extra.Vinegar);
-//        }
-//        if(RightList.contains(extraList.get(9))) {
-//            System.out.println(Extra.Avocado);
-//        }
-//
-//
-//        System.out.println(Order.lineNumber);
-//        //System.out.println("working");
-//    }
-//
-//    @FXML
-//    void remove(ActionEvent event) {
-//        int selected_int = ingred2.getSelectionModel().getSelectedIndex();
-//        if(selected_int > -1) {
-//            RightList.remove(selected_int);
-//        }
-//
-//        ObservableList<String> observableList = FXCollections.observableList(RightList);
-//        // extras.remove(Extra.Lettuce);
-//
-//        ingred2.setItems(observableList);
-//
-//
-//    }
-//    @FXML
-//    void print_details(ActionEvent event) {
-//        try {
-//            FXMLLoader fxmlLoader = new FXMLLoader();
-//            fxmlLoader.setLocation(getClass().getResource("details.fxml"));
-//            /*
-//             * if "fx:controller" is not set in fxml
-//             * fxmlLoader.setController(NewWindowController);
-//             */
-//            Scene scene = new Scene(fxmlLoader.load(), 500, 500);
-//            Stage stage = new Stage();
-//            stage.setTitle("Order Details");
-//            stage.setScene(scene);
-//            stage.show();
-//        } catch (IOException e) {
-//            //  Logger logger = Logger.getLogger(getClass().getName());
-//            //  logger.log(Level.SEVERE, "Failed to create new Window.", e);
-//        }
-//
-//
-//    }
-//
-//    @FXML
-//    void sumbit_order(ActionEvent event) {
-//        System.out.println("working 3");
-//
-//        if(selectmeat.getValue() == meatsList.get(0)) {
-//            //   ImageView chicken_image = new ImageView(("C:\\Users\\jsham\\eclipse-workspace\\projectfour\\src\\meatType.chicken.jpg"));
-//            //	System.out.println("chicken man");
-//            Chicken chick = new Chicken();
-//            System.out.println(chick);
-//        }
-//        else if(selectmeat.getValue() == meatsList.get(1)) {
-//            System.out.println("beef man");
-//            Beef b = new Beef();
-//            System.out.println(b.basicIngredients() + b.toString() );
-//            System.out.println(b);
-//            System.out.println(b.basicIngredients() + " " + RightList);
-//
-//        }
-//        else{
-//            System.out.println("fish man");
-//            Fish f = new Fish();
-//            System.out.println(f);
-//
-//
-//        }
-//    }
-//
-//    @FXML
-//    void change_meat(ActionEvent event) {
-//
-//
-//        if(selectmeat.getValue() == meatsList.get(0)) {
-//
-//            Chicken chick = new Chicken();
-//            String man = Double.toString(PER_EXTRA);
-//            price.appendText("Price is $8.99" + man);
-//            System.out.println(chick.basicIngredients() + ", " + Extra.Avocado);
-//
-//
-//        }
-//        else if(selectmeat.getValue() == meatsList.get(0)) {
-//
-//            Beef b = new Beef();
-//            System.out.println(b.price());
-//
-//
-//
-//        }
-//        else {
-//            Fish f = new Fish();
-//
-//        }
-//
-//    }
-//
-//
-//    @FXML
-//    void initialize() {
-//        selectmeat.setValue(meatsList.get(0));
-//        selectmeat.setItems(meatsList);
-//        ingredients.setItems(extraList);
-//        //	ingredients.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
-//
-//    }
-//
-//    @Override
-//    public double price() {
-//        // TODO Auto-generated method stub
-//        return 0;
-//    }
-////
-//    @Override
-//    public String basicIngredients() {
-//        // TODO Auto-generated method stub
-//        return null;
-//    }
+    @FXML
+    void removeIngredients(ActionEvent event){
+        int extraListSize = extraObservableList.size();
+        extraObservableList.addAll(currentListView.getSelectionModel().getSelectedItems());
+        currentObservableList.removeAll(currentListView.getSelectionModel().getSelectedItems());
+        int itemsRemoved = extraObservableList.size() - extraListSize;
+        double priceChange = 1.99 * itemsRemoved;
+        price -= priceChange;
+        priceField.setText("$" + formatPrice(price));
+        return;
+    }
 
+    private String formatPrice(double price){
+        DecimalFormat decimalFormat = new DecimalFormat(pattern);
+        String numberStr = decimalFormat.format(price);
+        return numberStr;
+    }
+
+    @FXML
+    void changeSandwichType(ActionEvent event){
+        //No changes made
+        if(meatBox.getValue().toString().equals(prevSandwich)) return;
+
+        //Remove sandwich price of previous sandwich and adds price and updates image of choosen sandwich
+        if(meatBox.getValue().toString().equals("Chicken")){
+            if(prevSandwich.equals("Beef")){
+                price -= 10.99;
+            }else{
+                price -= 12.99;
+            }
+            prevSandwich = "Chicken";
+            price += 8.99;
+            sandwichView.setImage(chickenImage);
+        }else if(meatBox.getValue().toString().equals("Beef")){
+            if(prevSandwich.equals("Chicken")){
+                price -= 8.99;
+            }else{
+                price -= 12.99;
+            }
+            prevSandwich = "Beef";
+            price += 10.99;
+            sandwichView.setImage(beefImage);
+        }else{
+            if(prevSandwich.equals("Chicken")){
+                price -= 8.99;
+            }else {
+                price -= 10.99;
+            }
+            prevSandwich = "Fish";
+            price += 12.99;
+            sandwichView.setImage(fishImage);
+        }
+        priceField.setText("$" + formatPrice(price));
+    }
+
+    @FXML
+    void submitOrder(ActionEvent event){
+
+        if(meatBox.getValue().toString().equals("Chicken")){
+            Chicken sandwich = new Chicken();
+            for(int i=0; i < currentObservableList.size(); i++){
+                sandwich.add(currentObservableList.get(i));
+            }
+            double price = sandwich.price();
+            OrderLine orderLine = new OrderLine(orders, sandwich, price);
+            orders.add(orderLine);
+        }else if(meatBox.getValue().toString().equals("Beef")){
+            Beef sandwich = new Beef();
+            for(int i=0; i < currentObservableList.size(); i++){
+                sandwich.add(currentObservableList.get(i));
+            }
+            double price = sandwich.price();
+            OrderLine orderLine = new OrderLine(orders, sandwich, price);
+            orders.add(orderLine);
+        }else{
+            Fish sandwich = new Fish();
+            for(int i=0; i < currentObservableList.size(); i++){
+                sandwich.add(currentObservableList.get(i));
+            }
+            double price = sandwich.price();
+            OrderLine orderLine = new OrderLine(orders, sandwich, price);
+            orders.add(orderLine);
+        }
+        orderNumber = orders.getLineNumber();
+        orderNumberField.setText("" + orderNumber);
+        System.out.println(orders.printOrder());
+        defaultOrderSelection();
+    }
+
+    private void defaultOrderSelection(){
+        //Remove all current ingredient selections
+        extraObservableList.addAll(currentObservableList);
+        currentObservableList.clear();
+
+        //Set meat selection to default of Chicken if not already
+        if(!meatBox.getValue().toString().equals("Chicken")){
+            meatBox.setValue("Chicken");
+        }
+
+        //sets price to default of chicken with no ingredients added
+        price = 8.99;
+        priceField.setText("$" + formatPrice(price));
+        return;
+    }
+
+    @FXML
+    void orderDetails(ActionEvent event) throws Exception{
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("OrderDetails.fxml"));
+        Parent orderDetailsParent = loader.load();
+        Scene orderDetailsScene = new Scene(orderDetailsParent);
+
+        OrderDetailsController orderDetails = loader.getController();
+        orderDetails.start(orders);
+
+        Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+
+        window.setScene(orderDetailsScene);
+        window.show();
+    }
 }
-
